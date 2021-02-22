3f175a161dae62b5df71c99c618a94fa7b9b8978
Mateusz Dąbrowski <mat.dabrowski@yahoo.com>
2021-02-22 20:38:29 +0100

    module 19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,23 +1,36 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.NoTaskFindException;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks(){
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId){
-        return new TaskDto(1L, "test title", "test_content");
+    @RequestMapping(method = RequestMethod.GET, value = "getTask", params = "id")
+    public TaskDto getTask(@RequestParam("id") long taskId) throws NoTaskFindException {
+        //Optional<Task> task = service.getTaskById(taskId);
+
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId).orElseThrow(NoTaskFindException::new));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
